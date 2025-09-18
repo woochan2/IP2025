197d0233efc80c8f84f65ef9b25d6b36546c8384
woochan2 <95203164+woochan2@users.noreply.github.com>
2025-09-18 15:31:41 +0900

    Add files via upload

---
+++ chap141-1.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+img=cv2.imread('noisy2.png',0)
+#globalthresholding
+ret1,th1=cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+#Otsu'sthresholding
+ret2,th2=cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+#Otsu'sthresholdingafterGaussianfiltering
+blur=cv2.GaussianBlur(img,(5,5),0)
+ret3,th3=cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+#plotalltheimagesandtheirhistograms
+images=[img,0,th1,img,0,th2,blur,0,th3]
+titles=['OriginalNoisyImage','Histogram','GlobalThresholding(v=127)','OriginalNoisyImage','Histogram',"Otsu'sThresholding",'GaussianfilteredImage','Histogram',"Otsu'sThresholding"]
+for i in range(3):
+    plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+    plt.title(titles[i*3]),plt.xticks([]),plt.yticks([])
+    plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+    plt.title(titles[i*3+1]),plt.xticks([]),plt.yticks([])
+    plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+    plt.title(titles[i*3+2]),plt.xticks([]),plt.yticks([])
+plt.show()
---
+++ chap141.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+cap = cv2.VideoCapture('tracking2.avi')
+while(cap.isOpened()):
+ # Take each frame
+    ret, frame = cap.read()
+ # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+ # define range of blue color in HSV
+    lower_blue = np.array([10,128,128])
+    upper_blue = np.array([60,255,255])
+ # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+ # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+cv2.destroyAllWindows()
